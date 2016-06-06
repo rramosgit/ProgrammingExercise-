21db00f05575665c10fef266da732e535db55892
rramosgit <rramos10384@gmail.com>
2016-06-06 14:59:46 -0400

    Info
    a I couldn't read holidays
    b Find the way to reload datapicker without load the all page
    c With more time , read days from api and Valid fields with regular
    expression and put floating tooltip on holydays. Make a design for the
    page

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
+<?php
+class ApiCaller
+{
+    //some variables for the object
+    private $_app_id;
+    private $_app_key;
+    private $_api_url;
+     
+    //construct an ApiCaller object, taking an
+    //APP ID, APP KEY and API URL parameter
+    public function __construct($app_id, $app_key, $api_url)
+    {
+        $this->_app_id = $app_id;
+        $this->_app_key = $app_key;
+        $this->_api_url = $api_url;
+    }
+     
+    //send the request to the API server
+    //also encrypts the request, then checks
+    //if the results are valid
+    public function sendRequest($request_params)
+    {
+        //encrypt the request parameters
+        $enc_request = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->_app_key, json_encode($request_params), MCRYPT_MODE_ECB));
+         
+        //create the params array, which will
+        //be the POST parameters
+        $params = array();
+        $params['enc_request'] = $enc_request;
+        $params['app_id'] = $this->_app_id;
+         
+        //initialize and setup the curl handler
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->_api_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, count($params));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+ 
+        //execute the request
+        $result = curl_exec($ch);
+         
+        //json_decode the result
+        $result = @json_decode($result);
+         
+        //check if we're able to json_decode the result correctly
+       // if( $result == false || !isset($result['success'])  ) 
+	
+       if( $result == false || $result->success	== false)	
+		{
+            throw new Exception('Request was not correct');
+        }
+         
+        //if there was an error in the request, throw an exception
+        if( $result->success== false ) {
+            throw new Exception($result['errormsg']);
+        }
+         
+        //if everything went great, return the data
+        return $result->data;
+    }
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -16,8 +78,22 @@
 <script language="javascript" type="text/javascript" src="js/function_calendar_byme.js"></script>
 <script language="javascript">
 
-var holidayDates = ['7/4/2015','7/4/2016','7/4/2017','7/4/2018'];
 
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+	 
+	return xmlHttp.responseText;
+	//make a loop and take only dates, return it
+	
+}
+var holidayDates = ['7/4/2015','7/4/2016','7/4/2017','7/4/2018'];
 $(document).ready(function() {
 
 
@@ -26,12 +102,19 @@ $(document).ready(function() {
 
 	$(".next").click(function(){
 		
-	if($("#start_date").attr('value')=='' || $("#days").attr('value')=='')
+	if($("#start_date").attr('value')=='' || $("#days").attr('value')=='' || $("#countrycode").attr('value')=='')
 	{
 		alert('Please,fill the form above')
 	}	
 	else
 	{
+	  var countrycode = $("#countrycode").attr('value');
+      var url = "http://holidayapi.com/v1/holidays?country="+countrycode;
+	  
+	    //var holidayDates = httpGetAsync(url);
+	   
+	   
+	   
 	  var start_date = new Date($("#start_date").attr('value'));
 	  var days = parseInt($("#days").attr('value'))-1;
 	  var end_date = new Date(start_date);
@@ -55,12 +138,13 @@ $(document).ready(function() {
 <form>
 <input type="text" id="start_date" name="start_date" value="" />
 <input type="text" id="days" name="days" value="" />
+<input type="text" id="countrycode" name="countrycode" value="" />
 
 <input type="text" id="end_date" name="end_date" value="" readonly="readonly" class="hidden" />
-<label>"To Load Again click here !!"</label>
-<input type="submit" value="Reload">
-<a class="next"></a>
 
+<a class="next" ></a>
+<br class="clear;">
+<label>"To Load Again click here !!"</label><input type="submit" value="Reload">
 <div  id="calendar"  class="hidden" > </div>
 </form>
 </body>
--- js/tooltip.js
+++ js/tooltip.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 			
-          jQuery("span[tooltip]").mouseenter(function (e) {            
+          $(".holiday span").mouseenter(function (e) {            
             var posMouse = this.offsetLeft; 
-            var textTooltip = jQuery(this).attr("tooltip"); 
+            var textTooltip = $(this).attr("tooltip"); 
 			
             if (textTooltip.length > 0) {
-                 jQuery(this).append('<div class="tooltip"><div class="top_tp"></div><div class="content_tp">' + textTooltip + '</div><div class="bottom_tp"></div></div>');
+                 $(this).append('<div class="tooltip"><div class="top_tp"></div><div class="content_tp">' + textTooltip + '</div><div class="bottom_tp"></div></div>');
 			
                
-				jQuery("span > div.tooltip").css("top","50px");
-				jQuery("span > div.tooltip").css("left", "" + posMouse -20+ "px");
+				$("span > div.tooltip").css("top","50px");
+				$("span > div.tooltip").css("left", "" + posMouse -20+ "px");
 				
-                jQuery("span > div.tooltip").fadeIn(300);
+                $("span > div.tooltip").fadeIn(300);
             }
         });
       
